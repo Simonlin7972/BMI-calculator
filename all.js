24bc656efc7bfffe17c3406815c0851e99e42077
simon <a97210230@gmail.com>
2020-05-11 22:24:52 +0900

    update UI and js

--- all.js
+++ all.js
@@ -1,24 +1,38 @@
 //Elements
-var btnClose = document.querySelector('.ic-close');
-var btnStart = document.querySelector('.btnStart');
-var result = document.querySelector('.resultCard');
-var claMain = document.querySelector('.cal-Main');
+var btnClose = document.querySelector(".ic-close");
+var btnStart = document.querySelector(".btnStart");
+var result = document.querySelector(".resultCard");
+var claMain = document.querySelector(".cal-Main");
 
+// input element
+var heightInput = document.querySelector(".inputHeight");
+var weightInput = document.querySelector(".inputWeight");
+
+// var heightValue = heightInput.value;
+// var weightValue = weightInput.value;
 
 // Event
 
-btnClose.addEventListener('click', closeResult, false);
-btnStart.addEventListener('click', sendData, false);
+btnClose.addEventListener("click", closeResult, false);
+btnStart.addEventListener("click", sendData, false);
 
+function sendData() {
+  //UI transition
+  result.classList.remove("shrinkResult");
+  claMain.classList.remove("shrinkCal");
+  //get data
+}
 
-
-function sendData(e) {
-    result.classList.remove("shrinkResult");
-    claMain.classList.remove("shrinkCal");
+//算 BMI 值
+function calculation(heightValue, weightValue) {
+  var heightValue = heightInput.value;
+  var weightValue = weightInput.value;
+  var bmiHeight = (heightValue / 100) * (heightValue / 100);
+  var bmiValue = weightValue / bmiHeight;
 }
 
 function closeResult(e) {
-    // shrink animation
-    result.classList.add("shrinkResult");
-    claMain.classList.add("shrinkCal");
+  // shrink animation
+  result.classList.add("shrinkResult");
+  claMain.classList.add("shrinkCal");
 }
