df70f413a7bef611053dfa2caa1bda296d4770f4
simon <a97210230@gmail.com>
2020-05-12 21:23:03 +0900

    bmi comment color

--- all.js
+++ all.js
@@ -1,10 +1,13 @@
 //Elements
 var btnClose = document.querySelector(".ic-close");
 var btnStart = document.querySelector(".btnStart");
-var result = document.querySelector(".resultCard");
 var claMain = document.querySelector(".cal-Main");
+//bmi board
+var result = document.querySelector(".resultCard");
 var bmi = document.querySelector(".resultCard__bmi");
 var bmiText = document.querySelector(".resultCard__status__text");
+var bmiComment = document.querySelector(".resultCard__comment__text");
+var bmiCommentBox = document.querySelector(".resultCard__comment");
 
 // input element
 var heightInput = document.querySelector(".inputHeight");
@@ -37,26 +40,42 @@ function sendData() {
     case bmiValue < 18.5:
       bmi.style.color = "#36A1EF";
       bmiText.textContent = "體重過輕";
+      bmiComment.style.color = "#36A1EF";
+      bmiComment.textContent = "你太瘦了～請多吃一點！";
+      bmiCommentBox.style.border = "1px solid #36A1EF";
+
       break;
 
     case bmiValue > 18.5 && bmiValue < 24:
       bmi.style.color = "#0ABF57";
       bmiText.textContent = "非常理想";
+      bmiComment.style.color = "#0ABF57";
+      bmiComment.textContent = "做得很好，請繼續保持！";
+      bmiCommentBox.style.border = "1px solid #0ABF57";
       break;
 
     case bmiValue >= 24 && bmiValue < 30:
       bmi.style.color = "#C9991D";
       bmiText.textContent = "過重/輕度肥胖";
+      bmiComment.style.color = "#C9991D";
+      bmiComment.textContent = "要開始注意自己的飲食與體重哦！";
+      bmiCommentBox.style.border = "1px solid #C9991D";
       break;
 
     case bmiValue >= 30 && bmiValue < 35:
       bmi.style.color = "#F08D31";
       bmiText.textContent = "中度肥胖";
+      bmiComment.style.color = "#F08D31";
+      bmiComment.textContent = "要開始控制飲食及開始規律運動囉！";
+      bmiCommentBox.style.border = "1px solid #F08D31";
       break;
 
     case bmiValue >= 35:
       bmi.style.color = "#F24E07";
       bmiText.textContent = "重度肥胖";
+      bmiComment.style.color = "#F24E07";
+      bmiComment.textContent = "需要嚴格控制飲食及增加運動強度！";
+      bmiCommentBox.style.border = "1px solid #F24E07";
       break;
   }
 }
